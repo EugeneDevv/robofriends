cdf00f92f4e3ccfff1c01d892b7bc76137bf9875
EugeneDevv <eugenemrlens@gmail.com>
2020-06-26 02:07:51 +0300

    Test

---
+++ precache-manifest.a1f1ec85b8a9ed0ac81101d6b6bef7d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7ed6e5b384d65dbcc95803479e0f6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bfceb24f787c2c153bb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3206089bab05f8a0aff",
+    "url": "/static/js/2.1317703d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1317703d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bfceb24f787c2c153bb",
+    "url": "/static/js/main.4a6a1cff.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
